046707fd5b795b4e25498fc7da730fee7b5e70ef
jessica mota <jessicam.arq.urb@gmail.com>
2025-08-19 14:59:35 +0200

    setup message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_19_101942) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_19_124917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_101942) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.boolean "from_user"
+    t.bigint "chat_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_id"], name: "index_messages_on_chat_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -50,5 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_19_101942) do
   end
 
   add_foreign_key "chats", "users"
+  add_foreign_key "messages", "chats"
   add_foreign_key "recipes", "users"
 end
