9f66b559e38486cbce6c0d5b551646b3b284c5ee
jessica mota <jessicam.arq.urb@gmail.com>
2025-08-19 16:28:02 +0200

    review chat

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :recipes, only: [:create, :destroy]
+  resources :recipes
   resources :chats, except: [:edit, :update] do
     resources :messages, except: [:new]
   end
