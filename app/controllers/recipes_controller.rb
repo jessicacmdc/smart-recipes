c9ce157a21e517da77f66cacaf0516f329c7ec86
jessica mota <jessicam.arq.urb@gmail.com>
2025-08-19 12:56:44 +0200

    view recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,5 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [ :show, :destroy ]
-
-  def create
-   # THE AI will create this
-
-    if @recipe.save
-      redirect_to recipe_path(@recipe)
-    else
-      # render :new, status: :unprocessable_entity
-    end
-  end
-
+  before_action :set_recipe, only: [ :show, :edit, :destroy ]
 
   def index
     @recipes = Recipe.all
@@ -25,19 +14,11 @@ class RecipesController < ApplicationController
   def destroy
     @recipe.destroy
     redirect_to recipe_path(@recipe), status: :see_other
-
   end
 
   private
 
-    def set_recipe
+  def set_recipe
     @recipe = Recipe.find(params[:id])
   end
-
 end
-
-
-
-  # def review_params
-  #   params.require(:review).permit(:content)
-  # end
