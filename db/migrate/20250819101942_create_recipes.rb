31cc28b0292443eb1752cb2a2626a33d4add62e0
jessica mota <jessicam.arq.urb@gmail.com>
2025-08-19 12:20:39 +0200

    model recipe

---
+++ db/migrate/20250819101942_create_recipes.rb
@@ -0,0 +1,15 @@
+class CreateRecipes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :ingredients
+      t.string :instruction
+      t.string :category
+      t.integer :required_time
+      t.string :difficulty_level
+      t.string :serves
+
+      t.timestamps
+    end
+  end
+end
