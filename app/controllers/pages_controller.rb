8875eb89c5c9cac5991019bd0317a094573a574a
Nathalia Sevilha <nathaliasevilha@gmail.com>
2025-08-20 20:22:48 +0200

    Pages controller and application controller for redirection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
-  # redirect_to dashboard_path if user_signed_in?
+    redirect_to chats_path if user_signed_in?
   end
 end
